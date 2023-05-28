9d30ca07031d9c4a8300b6a561c753f9ac4f2974
Samuel Rocha <samuelro.net@gmail.com>
2023-05-28 20:34:23 -0300

    Initial Commit v.1.0.0.0

---
+++ OnovaWinFormsTests/AppNetFrameworkOnovaTest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AppNetFrameworkOnovaTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrentVersion = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Version";
+            // 
+            // lblCurrentVersion
+            // 
+            this.lblCurrentVersion.AutoSize = true;
+            this.lblCurrentVersion.Location = new System.Drawing.Point(12, 43);
+            this.lblCurrentVersion.Name = "lblCurrentVersion";
+            this.lblCurrentVersion.Size = new System.Drawing.Size(35, 13);
+            this.lblCurrentVersion.TabIndex = 1;
+            this.lblCurrentVersion.Text = "label2";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(16, 119);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Fechar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(205, 21);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "&Atualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblCurrentVersion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCurrentVersion;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ OnovaWinFormsTests/AppNetFrameworkOnovaTest/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Onova;
+using Onova.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppNetFrameworkOnovaTest
+{
+    public partial class Form1 : Form
+    {
+        private readonly IUpdateManager updateManager = new UpdateManager(
+                new GithubPackageResolver("orochasamuel", "onova-winforms-tests", "onova-winforms-tests-*.zip"),
+                new ZipPackageExtractor()
+            );
+
+        string currentVersion;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            currentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            lblCurrentVersion.Text = currentVersion;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Check for updates
+            var check = await updateManager.CheckForUpdatesAsync();
+
+            // If there are none, notify user and return
+            if (!check.CanUpdate)
+            {
+                MessageBox.Show("There are no updates available.");
+                return;
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show(
+                    $"There is an update available. Your current version is {currentVersion}, the latest version is {check.LastVersion}. Would you like to update to the latest version?",
+                    "Update Available",
+                    MessageBoxButtons.YesNo
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    // Prepare the latest update
+                    await updateManager.PrepareUpdateAsync(check.LastVersion);
+
+                    // Launch updater and exit
+                    updateManager.LaunchUpdater(check.LastVersion);
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
